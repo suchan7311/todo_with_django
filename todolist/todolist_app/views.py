883c6a7e998587ea4f7cc2a3e97dc20a74a27fee
hansuchan <suchan7311@naver.com>
2021-01-26 01:00:31 +0900

    add createTodo

--- todolist/todolist_app/views.py
+++ todolist/todolist_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 # Create your views here.
 def index(request):
     return render(request, 'todolist_app/index.html')
+
+def createTodo(request):
+    todoContent=request.POST['todoContent']
+    new_todo=Todo(content=todoContent)
+    new_todo.save()
+    return HttpResponseRedirect(reverse("index"))
